6f91d35b1081253c31876df86f6b2cb243ce69c9
KaanAydin54 <kaanaydin8754@gmail.com>
2024-10-14 20:13:41 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import random
+gercek = ['Benim adım muzafferdir','En sevdiğim yemek mantıdır','Oyun oynamayı severim'] 
+
+rasgele_bilgi = random.choice(gercek)
+print("Hakkımdaki gerçek:",rasgele_bilgi)
